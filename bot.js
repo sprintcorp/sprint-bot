9f312e0798d74a2615224c3312a75220d8028fb4
Frederick Adebayo <sprintcorp7@gmail.com>
2023-06-02 16:34:47 +0100

    bot init

---
+++ bot.js
@@ -0,0 +1,49 @@
+const Twit = require('twit');
+
+const client = new Twit({
+  consumer_key: 'Ek20wjJsWbQX0tRpHVcLnugJD',
+  consumer_secret: 'shWZakD2PXrlWSuLv3jrzU5Iq3FokxD8PoiqWcONxGxa9pXjuQ',
+  access_token: '596575112-7A2AyGiUeJcqFDbJJMhYcC1jz8UPqS5Gxvn4ctWL',
+  access_token_secret: 'iUvbaKqvbf56lFBxPRfLLyvelGGeLSY9OIQHG4NOKXcY3',
+});
+
+// Search for tweets mentioning your bot
+client.get('search/tweets', { q: '@SprintBot', count: 10 }, (err, data, response) => {
+    if (err) {
+      console.error('Error retrieving tweets:', err);
+      return;
+    }
+  
+    const tweets = data.statuses;
+    for (const tweet of tweets) {
+      const tweetId = tweet.id_str;
+      const screenName = tweet.user.screen_name;
+  
+      // Process the video (e.g., create or modify video)
+  
+      // Upload the processed video
+      client.postMediaChunked({ file_path: 'path/to/processed_video.mp4' }, (err, data, response) => {
+        if (err) {
+          console.error('Error uploading video:', err);
+          return;
+        }
+  
+        const mediaId = data.media_id_string;
+  
+        // Reply to the tweet with the processed video
+        client.post('statuses/update', {
+          status: `@${screenName} Here's your video!`,
+          in_reply_to_status_id: tweetId,
+          media_ids: [mediaId],
+        }, (err, data, response) => {
+          if (err) {
+            console.error('Error replying to tweet:', err);
+            return;
+          }
+  
+          console.log('Tweet replied with video successfully!');
+        });
+      });
+    }
+  });
+  
